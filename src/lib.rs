13e9956aebb6f84e35ac972e169a7eacaee13668
Kagami Sascha Rosylight <saschanaz@outlook.com>
2021-01-02 15:51:19 +0100

    Rename as libjxl-src, remove bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,31 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+pub fn out_dir() -> &'static str {
+  std::env!("OUT_DIR")
+}
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+pub fn print_cargo_link() {
+  let dst = out_dir();
+
+  #[cfg(all(windows, debug_assertions))]
+  // Prevents "undefined symbol _CrtDbgReport" linker error
+  println!("cargo:rustc-link-lib=dylib=msvcrtd");
+
+  println!("cargo:rustc-link-search=native={}/lib", dst);
+  println!("cargo:rustc-link-search=native={}/build/third_party", dst);
+  println!("cargo:rustc-link-search=native={}/build/third_party/brotli", dst);
+  println!("cargo:rustc-link-search=native={}/build/third_party/highway", dst);
+  println!("cargo:rustc-link-lib=static=jxl-static");
+  println!("cargo:rustc-link-lib=static=jxl_threads-static");
+  println!("cargo:rustc-link-lib=static=brotlicommon-static");
+  println!("cargo:rustc-link-lib=static=brotlidec-static");
+  println!("cargo:rustc-link-lib=static=brotlienc-static");
+  println!("cargo:rustc-link-lib=static=hwy");
+  println!("cargo:rustc-link-lib=static=skcms");
+}
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn test_print() {
+    super::print_cargo_link();
+  }
+}
