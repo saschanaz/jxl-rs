0c7959ea3a7a0363859bb4cf63bc6f1fb8b75a4a
Kagami Sascha Rosylight <saschanaz@outlook.com>
2021-01-10 22:02:05 +0100

    Wrap all enum values before type cast

--- libjxl-sys/tests/decode.rs
+++ libjxl-sys/tests/decode.rs
@@ -133,9 +133,9 @@ unsafe fn decode_oneshot(data: Vec<u8>) -> Result<(JxlBasicInfo, Vec<u8>, Vec<u8
 
     try_dec!(JxlDecoderSubscribeEvents(
         dec,
-        JxlDecoderStatus_JXL_DEC_BASIC_INFO
+        (JxlDecoderStatus_JXL_DEC_BASIC_INFO
             | JxlDecoderStatus_JXL_DEC_COLOR_ENCODING
-            | JxlDecoderStatus_JXL_DEC_FULL_IMAGE as i32
+            | JxlDecoderStatus_JXL_DEC_FULL_IMAGE) as i32
     ));
 
     let result = decode_loop(dec, data);
