c886c2e444852f6778a8cf181ef737a10f595b39
Kagami Sascha Rosylight <saschanaz@outlook.com>
2021-01-12 04:29:33 +0100

    Move out_path back to the use position

--- libjxl-sys/build.rs
+++ libjxl-sys/build.rs
@@ -10,8 +10,6 @@ fn main() {
     let include_dir = format!("{}/include", libjxl_src::out_dir());
     println!("cargo:include={}", include_dir);
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
@@ -35,6 +33,7 @@ fn main() {
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
