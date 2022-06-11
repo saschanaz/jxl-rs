1abbf055e9a1e184e1f6e5e49307d271c39244cd
Kagami Sascha Rosylight <saschanaz@outlook.com>
2022-06-11 13:00:05 +0200

    Adjust clippy configuration

--- libjxl-sys/src/lib.rs
+++ libjxl-sys/src/lib.rs
@@ -1,6 +1,5 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-// XXX: https://github.com/rust-lang/rust-bindgen/issues/1651
-#![allow(deref_nullptr)]
+#![allow(non_upper_case_globals)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
